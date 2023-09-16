eb01e05985b3683458293dd56d3a78ce458fdc47
unknown <aslan.thr@gmail.com>
2023-09-16 15:08:34 -0400

    Update project description.

--- src/routes/DigitalBinder.tsx
+++ src/routes/DigitalBinder.tsx
@@ -3,7 +3,7 @@ import ProjectTemplate from "../components/ProjectTemplate";
 import Tag from "../components/Tag";
 
 const DigitalBinder = () => {
-  const liveLink = "https://taheri.dev";
+  const liveLink = "https://github.com/AslanTaheri/portfolio";
   const githubLink = "https://github.com/AslanTaheri/portfolio";
   const title = "Digital Binder /";
   const image = <img src={imageSrc} />;
@@ -19,10 +19,12 @@ const DigitalBinder = () => {
   );
   const text = (
     <p>
-      The layout organizer that brings structure to your content. With the power
-      to automatically arrange your HTML elements and text into a clean and
-      reader-friendly layout, Digital Binder is here to simplify your content
-      presentation.
+      Digital Binder is a layout organizer that brings structure to your
+      content. With the power to automatically arrange your HTML elements and
+      text into a clean and reader-friendly layout, Digital Binder is here to
+      simplify your content presentation. I am developing this product using
+      Typescript and React, and currently my personal website uses this product
+      to present my projects.
     </p>
   );
   return (
